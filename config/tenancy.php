5fb843de3fe8839fecf217410e4655aab1579959
jean_j <contact@jonathanjean.fr>
2025-06-19 00:34:44 +0200

    Update

--- config/tenancy.php
+++ config/tenancy.php
@@ -16,9 +16,6 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        '127.0.0.1',
-        'localhost',
-        'save.test',
         'beta.save-telematique.fr'
     ],
 
