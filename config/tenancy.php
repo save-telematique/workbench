fb1f8008918b1d46370d4a72af613a31d032fe40
jean_j <contact@jonathanjean.fr>
2025-06-19 00:47:59 +0200

    Update

--- config/tenancy.php
+++ config/tenancy.php
@@ -16,8 +16,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'beta.save-telematique.fr',
-        'save.test',
+        env('APP_URL'),
     ],
 
     /**
